0c28be69fa4fd6562914c243c0d6373938b22074
saifmhr <saifmahar165@gmail.com>
2022-10-20 18:29:50 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using PATA = ProjectA.TeamA;
+using PATB = ProjectA.TeamB;
+
+namespace Name_Space_In_CSharp
+{
+  class Program
+    {
+        static void Main()
+        {
+            PATA.ClassA.Print();
+            PATB.ClassA.Print();
+
+
+
+            //ClassA.Print();
+            //ClassB.Print();
+        }
+    }
+}
+
+//namespace ProjectA
+//{
+//    namespace TeamA {
+//        class ClassA
+//        {
+//            public static void Print()
+//            {
+//                Console.WriteLine("Team A Print Method");
+//            }
+//        }
+//    }
+//}
+
+//namespace ProjectA
+//{
+//    namespace TeamB
+//    {
+//        class ClassB
+//        {
+//            public static void Print()
+//            {
+//                Console.WriteLine("Team B Print Method");
+//            }
+//        }
+//}
+
---
+++ ProjectA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+ namespace ProjectA.TeamA
+{
+    public class Program
+    {
+        static void Main()
+        {
+            ClassA.Print();
+        }
+    }
+}
